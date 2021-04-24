eba9e6b4a822b3ee10da97e7ac61680a68523797
hegphegp <2386824052@qq.com>
2021-04-24 12:26:48 +0800

    修改依赖

--- src/test/java/com/codingfly/AppTest.java
+++ src/test/java/com/codingfly/AppTest.java
@@ -2,19 +2,32 @@ package com.codingfly;
 
 import static org.junit.Assert.assertTrue;
 
+import com.codingfly.httpclient.model.HttpConfig;
+import com.codingfly.httpclient.model.HttpMethod;
+import com.codingfly.httpclient.util.HttpClientUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void test() {
+        HttpClient httpClient = HttpClients.custom().build();
+        HttpConfig config = new HttpConfig()
+                .setClient(httpClient)
+                .setUrl("https://www.baidu.com/")
+                .setMethod(HttpMethod.GET)
+                .setConnectionRequestTimeout(10000)
+                .setConnectTimeout(10000)
+                .setSocketTimeout(24*3600*1000);
+
+        System.out.println(HttpClientUtils.get(config).getResult());
+
     }
 }
